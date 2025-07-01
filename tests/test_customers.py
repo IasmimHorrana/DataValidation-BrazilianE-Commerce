7de331eb0cfa325e265293a61c8b0a05f02a233f
Iasmim Horrana <horranaiasmim@gmail.com>
2025-06-30 21:44:12 -0300

    test(transform): inicia testes com pytest para funções de transformação

--- tests/test_customers.py
+++ tests/test_customers.py
@@ -1,30 +1,41 @@
+import pandas as pd
 import pytest  # noqa: F401
+from pandas.testing import assert_frame_equal
 
-from src.extract import ler_csv_customers
-
-path = "data/olist_customers_dataset.csv"
-
-
-def test_carregamento_customers():
-    df = ler_csv_customers(path)
-    assert not df.empty, "O DataFrame está vazio!"
+from src.transform.customers import (
+    filtrar_customer_id,
+)
 
 
-def test_customer_id_existe_em_orders():  # Deve bater com orders
+def test_filtrar_customer_id():
+    # Arrange: DataFrame de teste com alguns valores válidos e inválidos
+    df_teste = pd.DataFrame(
+        {"customer_id": ["a" * 32, "b" * 30, "c" * 32], "valor": [1, 2, 3]}
+    )
+
+    # Esperado: apenas as linhas com customer_id de 32 caracteres
+    df_esperado = pd.DataFrame(
+        {"customer_id": ["a" * 32, "c" * 32], "valor": [1, 3]}, index=[0, 2]
+    )  # mantém os índices originais se a função não resetar o índice
+
+    # Act: executa a função
+    resultado = filtrar_customer_id(df_teste)
+
+    # Assert: compara com o DataFrame esperado
+    assert_frame_equal(resultado, df_esperado)
+
+
+def test_filtrar_customer_unique_id():
     pass
 
 
-def test_customer_unique_id_existe_em_orders():  # Deve bater com orders
+def test_filtrar_customer_zip_code_valido():
     pass
 
 
-def test_customer_zip_code_prefix_valido():  # Entre 10000 e 99999
+def test_filtrar_customer_city_valido():
     pass
 
 
-def test_customer_city_nao_nulo():  # Não-nulo
-    pass
-
-
-def test_customer_state_duas_letras():  # Deve ter duas letras (UF)
+def test_filtrar_customer_state_duas_letras():
     pass
