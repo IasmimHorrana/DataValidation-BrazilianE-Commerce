605fb4c1678fafb150d3760ea977b3b097f45c8a
Iasmim Horrana <horranaiasmim@gmail.com>
2025-07-14 13:06:38 -0300

    chore(transform): define funções públicas no __init__.py do módulo

---
+++ src/transform/__init__.py
@@ -0,0 +1,9 @@
+from .customers import transformar_customers
+from .order_items import transformar_order_items
+from .orders import transformar_orders
+
+__all__ = [
+    "transformar_customers",
+    "transformar_orders",
+    "transformar_order_items",
+]
